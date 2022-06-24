17be895105f6df6afb2a560a7e751e60149d1b0f
hibikinatsume <higanhashimoto@gmail.com>
2022-06-24 18:42:41 +0700

    Content Left - Access and shortcuts (success)

--- public/js/main.js
+++ public/js/main.js
@@ -19,7 +19,7 @@ function rightMenuClick(element) {
     const rightMenu = document.getElementsByClassName("right-menu");
 
     for (var i = 0; i < rightMenu.length; i++) {
-        rightMenu[i].classList.remove('active')
+        rightMenu[i].classList.remove('active');
     }
 
     const checkActive = element.classList.contains('active');
@@ -29,5 +29,44 @@ function rightMenuClick(element) {
     }
     console.log(element);
 }
+
+// Show more access content when click
+function moreAccessContent(button) {
+    const accessMore = document.getElementsByClassName("access-more");
+    const btnLess = document.getElementById("btn-less");
+
+    for (var i = 0; i < accessMore.length; i++) {
+        accessMore[i].classList.add('active');
+    }
+
+    btnLess.style.display = 'flex';
+    button.style.display = 'none';
+}
+
+// Hide less access content when click
+function lessAccessContent(button) {
+    const accessMore = document.getElementsByClassName("access-more");
+    const btnMore = document.getElementById("btn-more");
+
+    for (var i = 0; i < accessMore.length; i++) {
+        accessMore[i].classList.remove('active');
+    }
+
+    btnMore.style.display = 'flex';
+    button.style.display = 'none';
+}
+
+// Hover show edit button
+function showEditButton() {
+    const btnEdit = document.getElementById("btn-edit");
+    btnEdit.style.display = 'block';
+}
+
+// Hide edit button when not hover
+function hideEditButton() {
+    const btnEdit = document.getElementById("btn-edit");
+    btnEdit.style.display = 'none';
+}
+
 
 
